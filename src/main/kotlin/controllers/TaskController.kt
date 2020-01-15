e78a229594e3ccd0b566f84d94cbe554fa53484c
Ashok Gelal <ashokgelal@gmail.com>
2020-01-14 18:51:37 -0700

    Delete a task

--- src/main/kotlin/controllers/TaskController.kt
+++ src/main/kotlin/controllers/TaskController.kt
@@ -1,8 +1,12 @@
 package dev.alpas.fireplace.controllers
 
+import dev.alpas.fireplace.entities.Tasks
 import dev.alpas.fireplace.guards.CreateTaskGuard
 import dev.alpas.http.HttpCall
+import dev.alpas.orAbort
 import dev.alpas.routing.Controller
+import me.liuwj.ktorm.dsl.delete
+import me.liuwj.ktorm.dsl.eq
 
 class TaskController : Controller() {
     fun store(call: HttpCall) {
@@ -10,5 +14,11 @@ class TaskController : Controller() {
             val task = commit()
             call.replyAsJson(task)
         }
+    }
+
+    fun delete(call: HttpCall) {
+        val id = call.paramAsLong("id").orAbort()
+        Tasks.delete { it.id eq id }
+        call.acknowledge()
     }
 }
